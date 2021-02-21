d3cd1525530babda647bf2c7e71db5ee17703876
Quartz1106 <79353160+Quartz1106@users.noreply.github.com>
2021-02-21 15:52:05 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,29 +1,10 @@
-from discord.ext import commands
-import os
-import traceback
-
-bot = commands.Bot(command_prefix='/')
-token = os.environ['DISCORD_BOT_TOKEN']
-
-
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+import discord
+client = discord.Client()
 
 @client.event
-async def on_message(message):
-    # メッセージ送信者がBotだった場合は無視する
-    if message.author.bot:
-        return
-    # 「/neko」と発言したら「にゃーん」が返る処理
-    if message.content == '/neko':
-        await message.channel.send('にゃーん')
+async def on_ready():
+    print("on_ready")
+    print(discord.__version__)
 
-@bot.command()
-async def test(ctx):
-    await ctx.send('テストOK')
-
-
-bot.run(token)
+client.run("ODEyNTgyNTA4MDc1NTQ4Njkz.YDC2bg._cIvjhmVhHsadUek6UucITI0910
+           ")
