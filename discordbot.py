e92421d91305cc384dd84b1349200b67aae46e4a
Quartz1106 <79353160+Quartz1106@users.noreply.github.com>
2021-02-21 15:52:24 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,10 +1,1 @@
-import discord
-client = discord.Client()
 
-@client.event
-async def on_ready():
-    print("on_ready")
-    print(discord.__version__)
-
-client.run("ODEyNTgyNTA4MDc1NTQ4Njkz.YDC2bg._cIvjhmVhHsadUek6UucITI0910
-           ")
